437beaf991e1427e03f1d8b48361e4861a4d4825
Sergej Khmara <sergej.khmara.01@privatbank.ua>
2022-01-21 15:56:26 +0200

    1

--- Task_5.js
+++ Task_5.js
@@ -15,72 +15,27 @@
 //arr.push(2);
 //console.log(arr[1]); // выведет число 2
 
-//var  arr = [1, 2, -4, 3, -9, -1, 7];
+var n, arr = [1, 2, -4, 3, -9, -1, 7];
 
-//for(var i=0;i< arr.length; i++){
-//console.log(arr[i])
-//    isPositive ()
-//}
-
-  function isPositive() {
-      let n = 0 ;
-      for (var i = 0; i < arguments.length; i++) {
-          if (typeof arguments[i] == "number") {
-              if (arguments[i] > 0 ){
-               n=  false
-
-              } else {
-                  n= true  }
-          } else {
-              throw new Error('parameter type is not a Number')
+let rez = [];
+ function isPositive(a) {
+     if (typeof (a) == 'number') {
+         if (a > 0){
+             return  true;
+         }else {
+          return  false;
          }
-      }
-      console.log(n);
-      return n;
+
+     } else {
+         throw new Error('parameter type is not a Number')}
  }
 
-
-
-
-
- let array = [1, 2, -4, 3, -9, -1, 7];
- let result = [];
-
- function PP(a) {
-
-     if (!isNaN(a)) {
-         return a > 0;
+ for (let element of arr) {
+     if (isPositive(element)) {
+         rez.push(element);
      }
  }
+ console.log(rez);
 
- for (let element of array) {
-     if (PP(element)) {
-         result.push(element);
-     }
- }
- console.log(result);
 
-// pos (1,2,3);
-//
-//  function pos () {
-//      for (item of arguments)  {
-//  console.log(item);
-//      }
-//  }
-//
-//let mas = [1, 2, 3];
-// //f(mas);
-//
-// f ([1,2,3]);
 
-// var arr = ['a', 'b', 'c', 'd', 'f'];
-// //f (1,2,3);
-// var i = 0, arr ;
-// function f(arr) {
-//     console.log(arr[i]);
-//     i++;
-//     if (i < arr.length) f(arr);
-//
-// }
-//  //f(arr);
-// console.log (arr)
